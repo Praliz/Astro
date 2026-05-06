1cc0279ec62f4628954434346fb8fad214d4970f
Praliz <d.thinggaard18@gmail.com>
2026-05-06 17:49:20 +0200

    C: spawned the ship

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
     while(True):
        log_state()
+       screen.fill("black")
+       player.draw(screen)
+       pygame.display.flip()
+       dt = clock.tick(60)/1000 
        for event in pygame.event.get():
         if(event.type ==pygame.QUIT):
                return
-        screen.fill("black")
-        pygame.display.flip()
+   
     
     #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     #print(f"Screen width: {SCREEN_WIDTH}")
