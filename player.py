71005f2eae3a390afb5822c49fd1708c1583e26e
Praliz <d.thinggaard18@gmail.com>
2026-05-09 10:35:20 +0200

    I:shooting nurfed

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH , PLAYER_TURN_SPEED , PLAYER_SPEED , PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH , PLAYER_TURN_SPEED , PLAYER_SPEED , PLAYER_SHOOT_SPEED , PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x, y ,PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt 
         return self.rotation
@@ -20,7 +21,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.cooldown -= dt
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -31,6 +32,7 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -44,6 +46,9 @@ class Player(CircleShape):
         pygame.draw.polygon(screen,"white",self.triangle(),LINE_WIDTH)
 
     def shoot(self):
+        if(self.cooldown>0):
+            return
+        else:self.cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x,self.position.y)
         direction = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         shot.velocity = direction
